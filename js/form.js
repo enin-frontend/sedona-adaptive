6ccee025d0850db82d29193a83e5e3baa208b40b
lockican <lockilocki97@mail.ru>
2019-04-06 00:47:39 +0300

    Исправление бага скрипта отправки формы

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
-let firstName = document.getElementById('input-first-name'),
+let form = document.querySelector('form'),
+    firstName = document.getElementById('input-first-name'),
     lastName = document.getElementById('input-last-name'),
     userNumber = document.querySelector('.form-input-text__number'),
     userMail = document.querySelector('.form-input-text__mail'),
@@ -7,22 +8,20 @@ let firstName = document.getElementById('input-first-name'),
     errorBlock = document.querySelector('.modal-error'),
     succesBlock = document.querySelector('.modal-succes');
 
-  btnModal.addEventListener('click', function(event){
-  for(let i = 0; i < popup.length; i++){
-    popup[i].classList.remove('form__error');
-    if(!popup[i].value){
-      event.preventDefault();
-      errorBlock.style.display = 'block';
-      popup[i].classList.add('form__error');
-    }
-    else{
-      event.preventDefault();
-      succesBlock.style.display = 'block';
-      popup[i].value = '';
-    }
-  }
-});
-
+    btnModal.addEventListener('click', function(event){
+      for(let i = 0; i < popup.length; i++){
+        popup[i].classList.remove('form__error');
+        if(!popup[i].value){
+          event.preventDefault();
+          errorBlock.style.display = 'block';
+          popup[i].classList.add('form__error');
+        }
+        else if(firstName.value && lastName.value && userMail.value && userNumber.value){
+          event.preventDefault();
+          succesBlock.style.display = 'block';
+        }
+      }
+    });
 
 errorBlock.addEventListener('click',function(event){
   if(event.target && event.target.classList.contains('modal__button')){
@@ -33,5 +32,6 @@ errorBlock.addEventListener('click',function(event){
 succesBlock.addEventListener('click',function(event){
   if(event.target && event.target.classList.contains('modal__button')){
     succesBlock.style.display = 'none';
+    form.submit();
   }
 });
